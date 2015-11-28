95feddf7ee55cfee6505e3c484e9562da6ff52fb
nabezokodaikon <nabezoko.daikon@gmail.com>
2015-11-29 01:03:44 +0900

    collect まで実装
    http://qiita.com/mtoyoshi/items/f68beb17710c3819697f#collect-%E8%A6%81executioncontext%E5%88%A5%E3%82%B9%E3%83%AC%E3%83%83%E3%83%89%E3%81%A7%E3%81%AE%E5%AE%9F%E8%A1%8C

---
+++ src/main/scala/Example3.scala
@@ -0,0 +1,96 @@
+package com.github.nabezokodaikon
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{ Await, Future }
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{ Success, Failure, Try }
+
+object Example3 {
+
+  def example1() = {
+    val f: Future[String] = Future("hello")
+    val ff: Future[Int] = f.map(_.size)
+    ff.onSuccess {
+      case r: Int => println(r)
+    }
+    Await.ready(ff, Duration.Inf)
+  }
+
+  def example2() = {
+    trait X
+    trait A
+    trait B extends A
+    case class Foo(i: Int) extends X with B
+
+    val f: Future[Foo] = Future { Foo(1) }
+    val fx: Future[X] = f
+    val fb: Future[B] = fx.mapTo[B]
+    val fa: Future[A] = fb
+  }
+
+  def example3() = {
+    val f: Future[Int] = Future[Int] { throw new Exception("Error"); 1 }
+    val fe = f.failed
+    fe.onSuccess {
+      case e => println(e.getMessage)
+    }
+    Await.ready(fe, Duration.Inf)
+  }
+
+  def example4() = {
+    val f1: Future[Int] = Future { Thread.sleep(2000); 1 }
+    val f2: Future[Int] = Future { Thread.sleep(1000); 2 }
+
+    val f: Future[Int] = f1.flatMap { n1: Int =>
+      val ff: Future[String] = f2.map { n2: Int =>
+        val sum = n1 + n2
+        s"Hello$sum"
+      }
+
+      // ff.onSuccess {
+      // case r => println(r)
+      // }
+
+      // Await.ready(ff, Duration.Inf)
+      ff
+    }.map { str =>
+      str.size
+    }
+
+    Await.ready(f, Duration.Inf)
+
+    println(f.value.get)
+  }
+
+  def example5() {
+    val f: Future[Int] = Future { 200 / 1 }
+    val ff: Future[Int] = f.transform(
+      { n => n * 2 },
+      { t => throw new Exception("Error!") }
+    )
+
+    ff.onComplete {
+      case Success(r) => println(r)
+      case Failure(r) => println(r)
+    }
+    Await.ready(ff, Duration.Inf)
+  }
+
+  def example6() {
+    val f: Future[Int] = Future { 6 }
+    val ff: Future[Int] = f.filter(_ % 2 == 0).map(_ * 2)
+    ff.onComplete {
+      case Success(r) => println(r)
+      case Failure(t) => println(t.getMessage)
+    }
+    Await.ready(ff, Duration.Inf)
+  }
+
+  def example7() {
+    val f: Future[Int] = Future { 6 }
+    val ff = f.collect {
+      case v: Int if (v % 2 == 0) => println(s"Hello $v")
+    }
+    Await.ready(ff, Duration.Inf)
+  }
+}
